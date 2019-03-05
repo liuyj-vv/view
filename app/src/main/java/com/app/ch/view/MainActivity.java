180b490b40a17ae3ca890088e0435ba2edb7a85a
liuyj <353381808@qq.com>
2019-03-05 10:04:04 +0800

    添加 recyclerview ，能显示多个 item

--- app/src/main/java/com/app/ch/view/MainActivity.java
+++ app/src/main/java/com/app/ch/view/MainActivity.java
@@ -2,12 +2,30 @@ package com.app.ch.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.ArrayMap;
+
+import com.app.ch.view.home.lable.HomeLableRecyclerviewAdapter;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    static String STR_SYSINFO = "系统信息";
+    static String STR_NET = "网络设置";
+    static String STR_DISPLAY = "显示设置";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String ,Object> map = new ArrayMap<>();
+        map.put(STR_SYSINFO, null);
+        map.put(STR_NET, null);
+        map.put(STR_DISPLAY, null);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.home_lable_recyclerview);
+        HomeLableRecyclerviewAdapter homeLableRecyclerview = new HomeLableRecyclerviewAdapter(map);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(homeLableRecyclerview);
     }
 }
---
+++ app/src/main/java/com/app/ch/view/home/lable/HomeLableRecyclerviewAdapter.java
@@ -0,0 +1,49 @@
+package com.app.ch.view.home.lable;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.app.ch.view.R;
+
+import java.util.Map;
+
+public class HomeLableRecyclerviewAdapter extends RecyclerView.Adapter<HomeLableRecyclerviewAdapter.VH> {
+    static String TAG = "HomeLableRecyclerviewAdapter";
+    Map<String,Object> map;
+
+    public HomeLableRecyclerviewAdapter(Map<String,Object> map) {
+        this.map = map;
+        Log.e(TAG, HomeLableRecyclerviewAdapter.class.getName() + "map.size()" + this.map.size());
+
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.home_lable_recyclerview_item, viewGroup, false);
+        return new VH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH vh, int i) {
+        vh.button.setText("" + i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.map != null ? this.map.size() : 0;
+    }
+
+    public class VH extends RecyclerView.ViewHolder {
+        Button button;
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.home_lable_btn);
+        }
+    }
+}
