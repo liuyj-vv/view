dc88a0f87be254bc8c5c0897b3a483ba6310fe79
liuyj <353381808@qq.com>
2019-03-07 11:26:50 +0800

    为 static、dhcp、wifi 都添加 fragment。且使用 replace 能正常切换

--- app/src/main/java/com/app/ch/view/fragment/FragmentNet.java
+++ app/src/main/java/com/app/ch/view/fragment/FragmentNet.java
@@ -12,19 +12,44 @@ import android.widget.Button;
 import com.app.ch.view.R;
 
 public class FragmentNet extends MyFragment {
-    Fragment fragment = new FragmentNet_ethernet();
-    Button button;
+    Fragment fragmentNet_static = new FragmentNet_static();
+    Fragment fragmentNet_dhcp_ = new FragmentNet_dhcp();
+    Fragment fragmentNet_wifi = new FragmentNet_wifi();
+    Button button_static;
+    Button button_dhcp;
+    Button button_wifi;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_net, container, false);
+        button_static = view.findViewById(R.id.btn_ip_static);
+        button_dhcp = view.findViewById(R.id.btn_ip_dhcp);
+        button_wifi = view.findViewById(R.id.btn_ip_wifi);
+        button_static.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_net, fragmentNet_static).commit();
+            }
+        });
+        button_dhcp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_net, fragmentNet_dhcp_).commit();
+            }
+        });
+        button_wifi.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_net, fragmentNet_wifi).commit();
+            }
+        });
+        getChildFragmentManager().beginTransaction().replace(R.id.fragment_net, fragmentNet_static).commit();
         return view;
     }
 
     @Override
     public void onHiddenChanged(boolean hidden) {
         if (!hidden && isFromHomeLable == false) {
-            button.requestFocus();
         }
         super.onHiddenChanged(hidden);
     }
---
+++ app/src/main/java/com/app/ch/view/fragment/FragmentNet_wifi.java
@@ -0,0 +1,35 @@
+package com.app.ch.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.app.ch.view.R;
+
+public class FragmentNet_wifi extends MyFragment {
+    static String TAG = "FragmentNet_static";
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_net_wifi, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+        }
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        Toast.makeText(getContext(), TAG+"中按下返回键", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
