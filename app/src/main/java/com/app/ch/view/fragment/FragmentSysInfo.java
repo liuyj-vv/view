c29c53d9ce52173c94194aad50ab7ccb299ae9c0
liuyj <353381808@qq.com>
2019-03-07 10:42:54 +0800

    搭建 sysinfo 显示框架

--- app/src/main/java/com/app/ch/view/fragment/FragmentSysInfo.java
+++ app/src/main/java/com/app/ch/view/fragment/FragmentSysInfo.java
@@ -15,6 +15,20 @@ public class FragmentSysInfo extends MyFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sysinfo, container, false);
+        refreshSysinfoToShow();
         return view;
+    }
+
+    private void refreshSysinfoToShow(){
+        //实现要显示的数据，显示到界面上
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            // 每一次重新显示界面都要刷新
+            refreshSysinfoToShow();
+        }
     }
 }
