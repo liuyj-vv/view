90a30c83b417a16ca02ef767dd4706a2ad9bf295
liuyj <353381808@qq.com>
2019-03-07 17:04:19 +0800

    保证获取焦点时，焦点在 view 上

--- app/src/main/java/com/app/ch/view/tools/IpEditeView.java
+++ app/src/main/java/com/app/ch/view/tools/IpEditeView.java
@@ -4,18 +4,16 @@ import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.app.ch.view.R;
 
-public class IpEditeView extends LinearLayout implements TextWatcher,View.OnClickListener,View.OnKeyListener {
+public class IpEditeView extends LinearLayout implements TextWatcher,View.OnClickListener,View.OnKeyListener,View.OnFocusChangeListener {
     static String TAG = IpEditeView.class.getSimpleName();
     private EditText edit1;
     private EditText edit2;
@@ -25,36 +23,54 @@ public class IpEditeView extends LinearLayout implements TextWatcher,View.OnClic
     public IpEditeView(Context context,  AttributeSet attrs) {
         super(context, attrs);
         view = LayoutInflater.from(context).inflate(R.layout.layout_ip_edit_view, this);
+
         edit1 = view.findViewById(R.id.edit1);
         edit2 = view.findViewById(R.id.edit2);
         edit3 = view.findViewById(R.id.edit3);
         edit4 = view.findViewById(R.id.edit4);
-
-        edit1.addTextChangedListener(this);
-        edit2.addTextChangedListener(this);
-        edit3.addTextChangedListener(this);
-        edit4.addTextChangedListener(this);
 
         init(context);
     }
 
     private void init(final Context context){
         view.setBackground(getResources().getDrawable(R.drawable.maker_btn_home_lable));
-        view.setFocusable(true);
+        setEditNoFocuseable();
 
+        view.setFocusable(true);
         view.setOnClickListener(this);
         edit1.setOnKeyListener(this);
         edit2.setOnKeyListener(this);
         edit3.setOnKeyListener(this);
         edit4.setOnKeyListener(this);
+
+        edit1.addTextChangedListener(this);
+        edit2.addTextChangedListener(this);
+        edit3.addTextChangedListener(this);
+        edit4.addTextChangedListener(this);
+
+        edit1.setOnFocusChangeListener(this);
+        edit2.setOnFocusChangeListener(this);
+        edit3.setOnFocusChangeListener(this);
+        edit4.setOnFocusChangeListener(this);
     }
 
+    // 保证获取焦点时，焦点在 view 上
+    private void setEditNoFocuseable() {
+        edit1.setFocusable(false);
+        edit2.setFocusable(false);
+        edit3.setFocusable(false);
+        edit4.setFocusable(false);
+    }
+    private void setEditHasFocuseable() {
+        edit1.setFocusable(true);
+        edit2.setFocusable(true);
+        edit3.setFocusable(true);
+        edit4.setFocusable(true);
+    }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        if(edit1.getId() == getId()) {
 
-        }
     }
 
     @Override
@@ -69,16 +85,15 @@ public class IpEditeView extends LinearLayout implements TextWatcher,View.OnClic
 
     @Override
     public void onClick(View v) {
-
-        if(v.getId() == getRootView().getId()) {
+        if(v == view) {
             Toast.makeText(getContext(), "编辑状态", Toast.LENGTH_SHORT).show();
+            setEditHasFocuseable();
             edit1.requestFocus();
         }
     }
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
-
         if(v == edit1
             || v == edit2
             || v == edit3
@@ -88,5 +103,12 @@ public class IpEditeView extends LinearLayout implements TextWatcher,View.OnClic
             }
         }
         return false;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if(!hasFocus) {
+            setEditNoFocuseable();
+        }
     }
 }
